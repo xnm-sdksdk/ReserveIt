ab28b7e318dc8ff53d87831547a0777b22c998ce
xnm <n.mendonca@protonmail.com>
2025-12-26 15:41:16 +0000

    availability entity

--- services/nodejs/availability-service/src/entity/availability.entity.js
+++ services/nodejs/availability-service/src/entity/availability.entity.js
@@ -1,7 +1,44 @@
 import mongoose from "mongoose";
 
-const availabilityEntity = new mongoose.Schema({
+const availabilityEntity = new mongoose.Schema(
+    {
+        resourceId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            index: true,
+        },
+        resourceType: {
+            type: String,
+            required: true,
+            enum: ["computer", "room", "other"],
+        },
+        start: {
+            type: Date,
+            required: true,
+        },
+        end: {
+            type: Date,
+            required: true,
+        },
+        reason: {
+            type: String,
+            trim: true,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+        },
+    },
+    { timestamps: true }
+);
 
-})
+availabilitySchema.index({ resourceId: 1, start: 1, end: 1 });
 
 export default mongoose.model("Availability", availabilityEntity);
